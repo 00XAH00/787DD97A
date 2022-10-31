b057f2042fd46bf7132967eec3ddaa27d86e62e0
DEN zabr <zabrodin_2017@mail.ru>
2022-10-31 21:09:56 +0300

    add comments in class Flats (for XAH) - over worked

--- 787DD97A/Program.cs
+++ 787DD97A/Program.cs
@@ -2,15 +2,17 @@
 using CalcPriceOfFlat;
 using System.Collections;
 using System.Collections.Generic;
-// LocationFlat,NumbersOfRoom,MarketSegment,NumberOfStoreys,WallMaterial,FloorLocation,ApartmentArea,KitchentArea,balcony,DistanseFromMetroStation,Price,repair
-Flat flat = new Flat("", 2, "Современное жилье", 22, "", 19, 58.7, 11, true, 11, "Эконом");//21 300 000
+//
+// NumberOfStoreys,  FloorLocation, ApartmentArea,  KitchentArea, balcony, DistanseFromMetroStation,  repair
+
+Flat flat = new Flat( 22, 18, 58.7, 11, true, 11, "Эконом");//21 300 000
 
 Flats[] arrayflats = new Flats[3];
-arrayflats[0] = new Flats("", 2, "Современное жилье", 22, "", 20, 58.7, 11, true, 11, 2130000, "Эконом");
+arrayflats[0] = new Flats(20, 14, 58.7, 11, true, 11, 21300000, "Эконом");
 
-arrayflats[1] = new Flats("", 2, "Современное жилье", 22, "", 19, 57.2, 9, true, 11, 2250000, "Эконом");
+arrayflats[1] = new Flats(20, 19, 57.2, 9, true, 11, 22500000, "Эконом");
 
-arrayflats[2] = new Flats("", 2, "Современное жилье", 17, "", 15, 58.6, 14, true, 10, 2140000, "Эконом");
+arrayflats[2] = new Flats(18, 15, 58.6, 14, true, 10, 21400000, "Эконом");
 
 
 

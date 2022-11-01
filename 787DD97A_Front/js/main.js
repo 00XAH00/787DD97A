95211ddb1015d288f803cfff7c35138656dfa1f0
Mountain-bear7 <doluxan010406@gmail.com>
2022-11-02 02:00:49 +0300

    I made the left block of the calculator

--- 787DD97A_Front/js/main.js
+++ 787DD97A_Front/js/main.js
@@ -23,12 +23,83 @@ const close_menu_btn = document.getElementById("close_menu_btn")
 menu_btn.addEventListener("click", menu_visibl)
 close_menu_btn.addEventListener("click", menu_hide)
 
-
 //ползунок для калькулятора
-const rangeSlider = document.getElementById('range-slider');
+const roomRangeSlider = document.getElementById('room-rangeSlider');
 
-if (rangeSlider) {
-    noUiSlider.create(rangeSlider, {
+if (roomRangeSlider) {
+    noUiSlider.create(roomRangeSlider, {
+        start: [1, 15],
+        connect: true,
+        step: 1,
+        range: {
+            'min': [1],
+            'max': [15]
+        }
+    });
+
+    const roomInput0 = document.getElementById('room-input0');
+    const roomInput1 = document.getElementById('room-input1');
+    const roomInputs = [roomInput0, roomInput1];
+
+    roomRangeSlider.noUiSlider.on('update', function(values, handle){
+        // console.log(handle)
+        roomInputs[handle].value = Math.round(values[handle]);
+    });
+
+    const setRangeSlider = (i, value) => {
+        let arr = [null, null];
+        arr[i] = value;
+
+        roomRangeSlider.noUiSlider.set(arr);
+    };
+
+    roomInputs.forEach((el, index) => {
+        el.addEventListener('change', (e) => {
+            setRangeSlider(index, e.currentTarget.value);
+        });
+    });
+}
+
+const floorRangeSlider = document.getElementById('floor-rangeSlider');
+
+if (floorRangeSlider) {
+    noUiSlider.create(floorRangeSlider, {
+        start: [2, 100],
+        connect: true,
+        step: 1,
+        range: {
+            'min': [2],
+            'max': [100]
+        }
+    });
+
+    const floorInput0 = document.getElementById('floor-input0');
+    const floorInput1 = document.getElementById('floor-input1');
+    const floorInputs = [floorInput0, floorInput1];
+
+    floorRangeSlider.noUiSlider.on('update', function(values, handle){
+        // console.log(handle)
+        floorInputs[handle].value = Math.round(values[handle]);
+    });
+
+    const setRangeSlider = (i, value) => {
+        let arr = [null, null];
+        arr[i] = value;
+
+        floorRangeSlider.noUiSlider.set(arr);
+    };
+
+    floorInputs.forEach((el, index) => {
+        el.addEventListener('change', (e) => {
+            setRangeSlider(index, e.currentTarget.value);
+        });
+    });
+}
+
+const flatRangeSlider = document.getElementById('flat-rangeSlider');
+
+if (flatRangeSlider) {
+    noUiSlider.create(flatRangeSlider, {
         start: [10, 130],
         connect: true,
         step: 1,
@@ -38,23 +109,59 @@ if (rangeSlider) {
         }
     });
 
-    const input0 = document.getElementById('input-0');
-    const input1 = document.getElementById('input-1');
-    const inputs = [input0, input1];
+    const flatInput0 = document.getElementById('flat-input0');
+    const flatInput1 = document.getElementById('flat-input1');
+    const flatInputs = [flatInput0, flatInput1];
 
-    rangeSlider.noUiSlider.on('update', function(values, handle){
+    flatRangeSlider.noUiSlider.on('update', function(values, handle){
         // console.log(handle)
-        inputs[handle].value = Math.round(values[handle]);
+        flatInputs[handle].value = Math.round(values[handle]);
     });
 
     const setRangeSlider = (i, value) => {
         let arr = [null, null];
         arr[i] = value;
 
-        rangeSlider.noUiSlider.set(arr);
+        floorRangeSlider.noUiSlider.set(arr);
     };
 
-    inputs.forEach((el, index) => {
+    flatInputs.forEach((el, index) => {
+        el.addEventListener('change', (e) => {
+            setRangeSlider(index, e.currentTarget.value);
+        });
+    });
+}
+
+const kitchenRangeSlider = document.getElementById('kitchen-rangeSlider');
+
+if (kitchenRangeSlider) {
+    noUiSlider.create(kitchenRangeSlider, {
+        start: [2, 15],
+        connect: true,
+        step: 1,
+        range: {
+            'min': [2],
+            'max': [15]
+        }
+    });
+
+    const kitchenInput0 = document.getElementById('kitchen-input0');
+    const kitchenInput1 = document.getElementById('kitchen-input1');
+    const kitchenInputs = [kitchenInput0, kitchenInput1];
+
+    kitchenRangeSlider.noUiSlider.on('update', function(values, handle){
+        // console.log(handle)
+        kitchenInputs[handle].value = Math.round(values[handle]);
+    });
+
+    const setRangeSlider = (i, value) => {
+        let arr = [null, null];
+        arr[i] = value;
+
+        floorRangeSlider.noUiSlider.set(arr);
+    };
+
+    kitchenInputs.forEach((el, index) => {
         el.addEventListener('change', (e) => {
             setRangeSlider(index, e.currentTarget.value);
         });

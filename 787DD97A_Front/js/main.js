f22d7b86c2b2285bf712949152be3cb623ae7e67
Mountain-bear7 <doluxan010406@gmail.com>
2022-11-01 23:27:04 +0300

    created a range slider

--- 787DD97A_Front/js/main.js
+++ 787DD97A_Front/js/main.js
@@ -1,3 +1,4 @@
+//всплывающее меню
 function menu_visibl() {
     const content = document.getElementById("header-content")
     const menu = document.getElementById('menu')
@@ -21,3 +22,41 @@ const close_menu_btn = document.getElementById("close_menu_btn")
 
 menu_btn.addEventListener("click", menu_visibl)
 close_menu_btn.addEventListener("click", menu_hide)
+
+
+//ползунок для калькулятора
+const rangeSlider = document.getElementById('range-slider');
+
+if (rangeSlider) {
+    noUiSlider.create(rangeSlider, {
+        start: [10, 130],
+        connect: true,
+        step: 1,
+        range: {
+            'min': [10],
+            'max': [130]
+        }
+    });
+
+    const input0 = document.getElementById('input-0');
+    const input1 = document.getElementById('input-1');
+    const inputs = [input0, input1];
+
+    rangeSlider.noUiSlider.on('update', function(values, handle){
+        // console.log(handle)
+        inputs[handle].value = Math.round(values[handle]);
+    });
+
+    const setRangeSlider = (i, value) => {
+        let arr = [null, null];
+        arr[i] = value;
+
+        rangeSlider.noUiSlider.set(arr);
+    };
+
+    inputs.forEach((el, index) => {
+        el.addEventListener('change', (e) => {
+            setRangeSlider(index, e.currentTarget.value);
+        });
+    });
+}

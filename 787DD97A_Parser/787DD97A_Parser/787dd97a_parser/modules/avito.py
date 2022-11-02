d4b67ed8b997c5e486842b036a16cd9cea291762
00XAH00 <pxaxulin@gmail.com>
2022-11-02 12:06:21 +0300

    change parser to bs4

--- 787DD97A_Parser/787DD97A_Parser/787dd97a_parser/modules/avito.py
+++ 787DD97A_Parser/787DD97A_Parser/787dd97a_parser/modules/avito.py
@@ -1,6 +1,8 @@
 import requests, time, threading
 from bs4 import BeautifulSoup
 
+# from user_agent import generate_user_agent, generate_navigator
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -11,25 +13,28 @@ class avito():
         self.URL = "https://www.avito.ru/moskva/kvartiry/prodam/novostroyka-ASgBAQICAUSSA8YQAUDmBxSOUg"
 
         self.opts = Options()
+        self.opts.add_argument("--headless")
+        self.opts.add_argument(f"user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36")
         self.caps = DesiredCapabilities().CHROME
         self.caps["pageLoadStrategy"] = "eager"  #  interactive
-        self.opts.add_argument("--headless")
         self.driver = webdriver.Chrome('/Users/xah/Projects/787DD97A_calc/787DD97A_Parser/787DD97A_Parser/787dd97a_parser/modules/chromedriver', options=self.opts, desired_capabilities=self.caps)
 
 
-    def get_links(self, page:int) -> BeautifulSoup:
+    def get_links(self) -> BeautifulSoup:
         apartment_links = []
         def thread_links_get(start:int, end:int):
             driver = webdriver.Chrome('/Users/xah/Projects/787DD97A_calc/787DD97A_Parser/787DD97A_Parser/787dd97a_parser/modules/chromedriver', options=self.opts, desired_capabilities=self.caps)
             for page in range(start, end):
                 driver.get(f"{self.URL}?p={page}")
+                source = driver.page_source
+                soup = BeautifulSoup(source, 'lxml')
 
-                tracks = driver.find_elements(By.CLASS_NAME, 'iva-item-content-rejJg') # Находим блок с квартирой
-                # print(page)
-                for item in tracks:
-                    links = item.find_elements(By.CLASS_NAME, 'link-link-MbQDP') # Находим ссылки
-                    apartment_links.append(links[0].get_property('href'))
-                    # print(links[0].get_property('href'))
+                print(page)
+                apartments_div = soup.find_all('div', class_='iva-item-content-rejJg') # Находим блок с квартирой
+                for item in apartments_div:
+                    link = item.find_all('a', class_='link-link-MbQDP')[0].get('href') # Находим ссылку
+                    apartment_links.append(f"https://www.avito.ru{link}")
+
             driver.quit()
 
         self.driver.get(self.URL)
@@ -37,6 +42,7 @@ class avito():
         self.driver.quit()
 
 
+        # thread_first = threading.Thread(target=thread_links_get, args=(1, 2))
         thread_first = threading.Thread(target=thread_links_get, args=(1, (last_page//3)))
         thread_second = threading.Thread(target=thread_links_get, args=((last_page//3), (last_page//3)*2))
         thread_third = threading.Thread(target=thread_links_get, args=((last_page//3)*2, last_page))
@@ -51,7 +57,7 @@ class avito():
         thread_third.join()
         print("--- %s seconds ---" % (time.time() - start_time))
         #
-        # Потоки не доделаны, почитать про многопоточность selenium + ускорение selenium
+        # Потоки не доделаны, почитать про ускорение bs4
         #
 
 

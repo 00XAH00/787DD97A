a1936f405dbe4c90fb29ec0aa78edb11b7988864
Mountain-bear7 <doluxan010406@gmail.com>
2022-11-06 01:32:43 +0300

    I made a personal account header

---
+++ 787DD97A_Front/js/login.js
@@ -0,0 +1,24 @@
+//всплывающее меню
+function menu_visibl() {
+    // const content = document.getElementById("header-content")
+    const menu = document.getElementById('menu')
+
+    // content.style.visibility = "hidden"
+    menu_btn.style.visibility = "hidden"
+    menu.style.left = 0
+}
+function menu_hide() {
+    const menu = document.getElementById('menu')
+    // const content = document.getElementById("header-content")
+    
+    // content.style.visibility = "visible"
+    menu_btn.style.visibility = "visible"
+    menu.style.left = "-100%"
+}
+
+
+const menu_btn = document.getElementById("menu_btn")
+const close_menu_btn = document.getElementById("close_menu_btn")
+
+menu_btn.addEventListener("click", menu_visibl)
+close_menu_btn.addEventListener("click", menu_hide)

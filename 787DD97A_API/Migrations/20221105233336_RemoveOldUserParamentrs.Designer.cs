6429d984dfd91c8a1d0487dacd51d8bb2b7b3dc8
00XAH00 <pxaxulin@gmail.com>
2022-11-06 02:34:54 +0300

    Remove unused users parametrs

--- 787DD97A_API/Migrations/20221105233336_RemoveOldUserParamentrs.Designer.cs
+++ 787DD97A_API/Migrations/20221105233336_RemoveOldUserParamentrs.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using _787DD97A_API.Classes;
 
@@ -10,9 +11,10 @@ using _787DD97A_API.Classes;
 namespace _787DD97A_API.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20221105233336_RemoveOldUserParamentrs")]
+    partial class RemoveOldUserParamentrs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -54,9 +56,6 @@ namespace _787DD97A_API.Migrations
 
                     b.Property<uint>("Material")
                         .HasColumnType("int unsigned");
-
-                    b.Property<ulong>("Price")
-                        .HasColumnType("bigint unsigned");
 
                     b.Property<uint>("Rooms")
                         .HasColumnType("int unsigned");
---
+++ 787DD97A_API/Migrations/20221105233336_RemoveOldUserParamentrs.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _787DD97A_API.Migrations
+{
+    public partial class RemoveOldUserParamentrs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Login",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Patronymic",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Phone",
+                table: "Users",
+                newName: "IX_Users_Email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "Phone");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                newName: "IX_Users_Phone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "Users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Patronymic",
+                table: "Users",
+                type: "varchar(30)",
+                maxLength: 30,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+        }
+    }
+}

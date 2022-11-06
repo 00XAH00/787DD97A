fbd7458df102335649d31e9d9198c7ab1b25b0f2
00XAH00 <pxaxulin@gmail.com>
2022-11-06 04:42:00 +0300

    add function for refresh token

---
+++ 787DD97A_API/Models/TokenRefreshModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace _787DD97A_API.Models
+{
+    public class TokenRefreshModel
+    {
+        public Guid uuid { get; set; } = Guid.Empty;
+        public string DeviceId { get; set; } = string.Empty;
+    }
+}
+

46bf010054ff30afea7d1e539768341e22a2361e
den-Master-2040 <107744290+den-Master-2040@users.noreply.github.com>
2022-12-01 20:35:42 +0300

    add losting files

---
+++ 787DD97A_API/Classes/ExcelRW.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML;
+using ClosedXML.Excel;
+
+using Microsoft.AspNetCore.Mvc;
+using _787DD97A_API.Classes;
+using _787DD97A_API.Models;
+
+using CalcForPriceFlat;
+using CalcPriceOfFlat;
+
+namespace CalcForPriceFlat
+{
+    public class ExcelRW
+    {
+        //возвращает кварты из Excel файла namefile
+        public static Apartment[] GetFlatsForExcel(string namefile)
+        {
+            IXLWorkbook wb = new XLWorkbook(namefile);
+
+            IXLWorksheet ws = wb.Worksheets.Worksheet(1);//floor
+
+            int count_flat = 1;
+            
+            while(true)//подсчёт количества квартир
+            {
+                object value2 = ws.Cell(count_flat+1,1).Value;
+                if ((string)value2 == "" )
+                {
+                    break;
+                }
+                else
+                {
+                    count_flat++;
+                }
+            }
+            count_flat--;//квартиры всегда получаются на 1 больше
+
+            Apartment[] flat = new Apartment[count_flat];
+            object value;
+            for (int i = 0; i< count_flat; i++)
+            {
+                flat[i] = new Apartment();
+
+
+                object Adress = ws.Cell(2 + i, 1).Value;
+
+                flat[i].Adress = (string)Adress;
+
+
+                object Rooms = ws.Cell(2 + i, 2).Value;
+
+                flat[i].Rooms = Convert.ToUInt16(Rooms);
+
+
+                object Segment = ws.Cell(2 + i, 3).Value;
+
+                flat[i].Segment = Convert.ToUInt16(Segment);
+
+
+
+                object House_floors = ws.Cell(2 + i, 4).Value;
+               
+                flat[i].House_floors = Convert.ToUInt16(House_floors);
+                
+                
+                object Material = ws.Cell(2 + i, 5).Value;
+
+                flat[i].Material = Convert.ToUInt16(Material);
+
+                object Apartment_floor = ws.Cell(2 + i, 6).Value;
+               
+                flat[i].Apartment_floor = Convert.ToUInt16(Apartment_floor);
+
+
+
+                object Apatments_area = ws.Cell(2 + i, 7).Value;
+
+                flat[i].Apatments_area = Convert.ToUInt16(Apatments_area);
+
+
+
+                object Kitchen_area = ws.Cell(2 + i, 8).Value;
+
+                flat[i].Kitchen_area = (ushort)(Convert.ToInt32(Kitchen_area));
+
+
+
+                object Balcon = ws.Cell(2 + i, 9).Value;
+
+                if (Balcon == "Да")
+                {
+                    flat[i].Balcony = true;
+                }
+                else
+                {
+                    flat[i].Balcony = false;
+                }
+
+
+
+                object distanceForMetro = ws.Cell(2 + i, 10).Value;
+
+                flat[i].Undeground_minutes = (ushort)(Convert.ToInt32(distanceForMetro));
+
+
+
+
+                object repair = ws.Cell(2 + i, 11).Value;
+
+                flat[i].Condition = Convert.ToString(repair);
+
+                flat[i].Undeground = "";
+                flat[i].Link = "";
+            }
+            
+            
+            wb.SaveAs(namefile);
+            
+            return flat;
+        }
+
+
+        public static void SetFlats(int i, string namefile,double price)
+        {
+            IXLWorkbook wb = new XLWorkbook(namefile);
+
+            IXLWorksheet ws = wb.Worksheets.Worksheet(1);//floor      
+
+            
+            ws.Cell(2 + i, 12).Value = price;
+            
+            wb.SaveAs(namefile);
+        }
+    }
+}
---
+++ 787DD97A_API/Classes/SortFlat.cs
@@ -0,0 +1,144 @@
+﻿
+using _787DD97A_API.Models;
+using CalcForPriceFlat;
+using System;
+namespace _787DD97A_API.Classes
+{
+    public class SortFlat
+    {
+
+        static public Flats[] SortFlats(IQueryable<Apartment> apartments, Apartment apartment )
+        {
+
+            /**/
+            Apartment[] ApartmentsArray =  apartments.ToArray();
+
+
+            int CounterFlat = 0;
+
+            for ( int i = 0; i < ApartmentsArray.Length; i++ )
+            {
+                if( ApartmentsArray[i] != null )
+                if (Math.Abs(ApartmentsArray[i].Undeground_minutes - apartment.Undeground_minutes) <= 35
+                    && ApartmentsArray[i].Segment == apartment.Segment
+                    && ApartmentsArray[i].Material == apartment.Material
+                    && ApartmentsArray[i].Rooms == apartment.Rooms
+                    && ApartmentsArray[i].House_floors == apartment.House_floors
+                    )
+                {
+                    CounterFlat++;
+                }
+                else
+                ApartmentsArray = DeleteForIndex(ApartmentsArray, i);//удаляем элементы 
+            }
+            for (int i = 0; i < ApartmentsArray.Length; i++)
+            {
+                if (ApartmentsArray[i] != null)
+                {
+
+
+                    georooords.Calc_Distance(ApartmentsArray[i].Adress, apartment.Adress);
+                    if (georooords.rasst <= 0.01)
+                    {
+                        CounterFlat++;
+                    }
+                    else
+                        ApartmentsArray = DeleteForIndex(ApartmentsArray, i);//удаляем элементы 
+                }
+                else break;
+            }
+            int sizeArrayFlats = 0;
+            for(int i = 0; i < ApartmentsArray.Length;i++)
+            {
+                if (ApartmentsArray[i] != null) sizeArrayFlats++;
+            }
+
+            Flats[] flats = new Flats[sizeArrayFlats];
+
+            if (ApartmentsArray.Length == 0) return flats;
+
+            
+            for (int i = 0; i < sizeArrayFlats; i++)
+            {
+                if (ApartmentsArray[i] != null)
+                {               
+                    flats[i] = new Flats();
+                    flats[i].DistanceFromMetroStation = (ushort)ApartmentsArray[i].Undeground_minutes;
+                    flats[i].NumberOfStoreys = (ushort)ApartmentsArray[i].House_floors;
+                    flats[i].FloorLocation = (ushort)ApartmentsArray[i].Apartment_floor;
+                    flats[i].ApartmentArea = (ushort)ApartmentsArray[i].Apatments_area;
+                    flats[i].KitchentArea = (ushort)ApartmentsArray[i].Kitchen_area;
+                    flats[i].balcony = ApartmentsArray[i].Balcony;
+                    flats[i].repair = ApartmentsArray[i].Condition;
+                    flats[i].Price = ApartmentsArray[i].Price;
+                }
+            }
+
+       
+
+            return flats;
+        }
+
+
+        public static Apartment[] DeleteForIndex(Apartment[] ApartmentsArray, int index)
+        {
+            Apartment[] apartments = new Apartment[ApartmentsArray.Length-1];
+            int j = 0;
+            for (int i = 0; i < ApartmentsArray.Length-1; i++)
+            {
+                if(i!=index)
+                {
+                    if (ApartmentsArray[i] != null)
+                    {
+                        apartments[j] = ApartmentsArray[i];
+                        j++;
+                    }                    
+                }
+            }
+            return apartments;
+        }
+
+        public static Flats[] DeleteForIndexFlat(Flats[] flat, int index)
+        {
+            Flats[] flats = new Flats[flat.Length - 1];
+            int j = 0;
+            for (int i = 0; i < flat.Length; i++)
+            {
+                if (i != index)
+                {
+                    flats[i] = flat[j];
+                    j++;
+                }
+            }
+            return flats;
+        }
+
+        public static Flat Convert(Apartment ApartmentsArray)
+        {
+            Flat flats = new Flat();
+            flats.DistanceFromMetroStation = (ushort)ApartmentsArray.Undeground_minutes;
+            flats.NumberOfStoreys = (ushort)ApartmentsArray.House_floors;
+            flats.FloorLocation = (ushort)ApartmentsArray.Apartment_floor;
+            flats.ApartmentArea = (ushort)ApartmentsArray.Apatments_area;
+            flats.KitchentArea = (ushort)ApartmentsArray.Kitchen_area;
+            flats.balcony = ApartmentsArray.Balcony;
+            flats.repair = ApartmentsArray.Condition;
+          
+            return flats;
+        }
+
+        public static Apartment Convert(Flat flats)
+        {
+           Apartment apartment = new Apartment();
+           apartment.Undeground_minutes = flats.DistanceFromMetroStation;
+           apartment.House_floors = flats.NumberOfStoreys;
+           apartment.Apartment_floor = flats.FloorLocation;
+           apartment.Apatments_area = (uint)flats.ApartmentArea;
+           apartment.Kitchen_area = flats.KitchentArea;
+           apartment.Balcony = flats.balcony;
+           apartment.Condition = flats.repair;
+
+            return apartment;
+        }
+    }
+}
